498997a8e015155168a144202570cf9a254c5b8d
SahayakSharma <sahayaksharma6@gmail.com>
2025-03-06 09:33:24 +0530

    debug

--- app/(protected)/receiveCall/page.tsx
+++ app/(protected)/receiveCall/page.tsx
@@ -41,6 +41,7 @@ export default function ReceiveCall(){
         pc.ontrack=(event)=>{
             console.log("Track Received !")
             if(secondPersonVideo.current){
+                console.log("setting second person video")
                 secondPersonVideo.current.srcObject=new MediaStream([event.track]);
             }
         }
